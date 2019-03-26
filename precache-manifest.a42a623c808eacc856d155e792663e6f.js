6430d0f895af144e4c924e44adf64714262810a8
LordBerbeross <mustafaberber020@gmail.com>
2019-03-26 17:42:14 +0300

    Updates

---
+++ precache-manifest.a42a623c808eacc856d155e792663e6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e15c8e28caf27106d019d270c1a6c778",
+    "url": "/Starwars/static/media/starwars.e15c8e28.jpg"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/Starwars/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "da2d03cc8553e5ee72f3",
+    "url": "/Starwars/static/js/runtime~main.a4c11e3c.js"
+  },
+  {
+    "revision": "fa0532165619e9ba970b",
+    "url": "/Starwars/static/js/main.18c242c3.chunk.js"
+  },
+  {
+    "revision": "269be658eed7798d6a44",
+    "url": "/Starwars/static/js/2.f5bd0275.chunk.js"
+  },
+  {
+    "revision": "fa0532165619e9ba970b",
+    "url": "/Starwars/static/css/main.0f04bccb.chunk.css"
+  },
+  {
+    "revision": "269be658eed7798d6a44",
+    "url": "/Starwars/static/css/2.2ae2f3af.chunk.css"
+  },
+  {
+    "revision": "0ce30e67ff059bb094b6b1b2a8e2b339",
+    "url": "/Starwars/index.html"
+  }
+];
